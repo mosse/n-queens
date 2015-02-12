b2a0325586f4d2594c71428a10e0a6768bf33f6c
mosse <mjemacdonald@gmail.com>
2015-02-11 16:56:29 -0800

    Implement findNRooksSolution()

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,54 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // Initialize solution board to empty array
+  var solution = []; //fixme
+  // Initialize counter
+  var counter = 0;
+
+  var board = new Board({n: n});
+
+  // Initialize recursive function
+  var recurse = function(board) {
+    // Initialize new board
+    var newBoard = board;
+
+    if (counter === n) {
+      solution = newBoard.rows().slice();
+      return;
+    }
+    // Base case: counter === n
+    //   Set board to solution array
+    //   Return
+    // Recursive case:
+    //   Iterate over new board
+    //     If element is open (< 1)
+    //       Push element into new board
+    //       Test against helper functions
+    //         If there are conflicts (true)
+    //           Pop out element
+    //         If there are no conflicts (false)
+    //           Increment counter
+    //           Push new board into recursive function
+    for (var row = 0; row < n; row++){
+      for (var col = 0; col < n; col++){
+        if(newBoard.rows()[row][col] < 1){
+          newBoard.rows()[row][col] = 1;
+          if (newBoard.hasAnyRowConflicts() || newBoard.hasAnyColConflicts()) {
+            newBoard.rows()[row][col] = 0; // equivalent to popping out value
+          } else {
+            // recursive function
+            counter++;
+            recurse(newBoard);
+          }
+        }
+      }
+    }
+  };
+
+  // Call recursive function on initial board
+  // Return solution
+  recurse(board);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
